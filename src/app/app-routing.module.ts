57d83f0fdf7d4f96e22206fe5657529e3e1f65d2
Juan Martinez <devjuanmartinez@gmail.com>
2025-10-21 11:06:16 +0200

    Login actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '',
     redirectTo: 'login',
@@ -16,8 +15,8 @@ const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
- 
+  },
+
   
 ];
 
